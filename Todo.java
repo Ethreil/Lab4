3ebe3038cc0872259571855350e6aeabccacd3c6
Ethreil <37450012+Ethreil@users.noreply.github.com>
2019-02-12 23:49:33 -0600

    Modified To-Do list , activities and time

--- Todo.java
+++ Todo.java
@@ -1,18 +1,28 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 13
+Last Modified_______: 2019-02-12
+OU ID_______________: 113410553
+OU EMAIL____________: Gracen.J.Ownby-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Finish Animal Equals Method
+2. Finish Equipment Equals Method
+3. Finish writing AnimalTest class to test Animal constructors
+4. Write EquipmentTest class to test Equipment constructors
+5. Write ZooTest class to test Zoo constructors
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 10
+2. 10
+3. 15
+4. 25
+5. 25
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 15
+2. 5
+3. 8
+4. 15
+5. 30
+
 */}
